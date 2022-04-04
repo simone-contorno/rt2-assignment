b103d8cbdc87c4dc133256ce2404ffb6ae42d1a5
Simone Contorno <83269499+simone-contorno@users.noreply.github.com>
2022-04-04 10:47:17 +0200

    Delete statistical_analysis directory

--- statistical_analysis/python_simulator/robot-sim/sr/robot/__init__.py
+++
@@ -1,7 +0,0 @@
-from .simulator import Simulator
-from .sim_robot import SimRobot, AlreadyHoldingSomethingException
-from .vision import (
-    MARKER_ARENA,
-    MARKER_TOKEN_GOLD,
-    MARKER_TOKEN_SILVER,
-)
